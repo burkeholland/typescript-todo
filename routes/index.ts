8138fb343db19f849f8faf58db8b3aec92fe2633
Burke Holland <burkeholland@gmail.com>
2023-03-07 16:08:38 -0600

    remove old tests

--- routes/index.ts
+++ routes/index.ts
@@ -1,38 +1,7 @@
-import express, { Router } from 'express';
-import Todo from '../models/Todo';
-import todoService from '../services/todoService';
+import express from 'express';
 
-const router: Router = express.Router();
+const router = express.Router();
 
-// write a route for every method in the todoService
-
-router.get('/', async (req, res) => {
-  const todos = await todoService.getTodos();
-  res.render('index', { todos });
-});
-
-router.post('/', async (req, res) => {
-  const newTodo: Todo = {
-    title: req.body.title,
-    isCompleted: false,
-  };
-  await todoService.addTodo(newTodo);
-  res.redirect('/');
-});
-
-router.post('/delete/:id', async (req, res) => {
-  await todoService.deleteTodoById(req.params.id);
-  res.redirect('/');
-});
-
-router.post('/update/:id', async (req, res) => {
-  const todoToUpdate: Todo = {
-    id: req.params.id,
-    title: req.body.title,
-    isCompleted: req.body.isCompleted === 'on',
-  };
-  await todoService.updateTodoById(req.params.id, todoToUpdate);
-  res.redirect('/');
-});
+// TODO: Add routes here
 
 export default router;
